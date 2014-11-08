8b1d7d0cfb633597cb49b0c4fea499b93cdf79d3
Christopher Bebry <invalidsyntax@gmail.com>
2014-11-07 21:42:36 -0500

    Several frontend additions and touchups.

--- static/js/zuluCtrl.js
+++ static/js/zuluCtrl.js
@@ -1,7 +1,7 @@
 var zuluApp = angular.module('zuluApp', []);
 
 zuluApp.controller('ZuluController', ['$scope', function($scope) {
-	//TODO refactor constants into service
+	//TODO refactor constants into service, or something
 	$scope.constants = {};
 	$scope.constants.dataTypes = [
 		{ name: "STRING" },
@@ -14,8 +14,21 @@ zuluApp.controller('ZuluController', ['$scope', function($scope) {
 	];
 	
 	$scope.currentModel = {};
+	$scope.currentModel.editMode = false;
 	$scope.currentModel.name = 'Example';
+	$scope.currentModel.id = 0;
 	$scope.currentModel.fields = [];
-	$scope.currentModel.fields.push({ name: 'name', type: $scope.constants.dataTypes[0] });
-	$scope.currentModel.fields.push({ name: 'description', type: $scope.constants.dataTypes[0] });
+	$scope.currentModel.fields.push({ name: 'name', type: $scope.constants.dataTypes[0], length: '10', allowNull: false });
+	
+	$scope.currentModel.toggleEditMode = function() {
+		$scope.currentModel.editMode = !$scope.currentModel.editMode;
+	}
+	
+	$scope.currentModel.addNewField = function() {
+		$scope.currentModel.fields.push({ name: '', type: $scope.constants.dataTypes[0], length: '' });
+	}
+	
+	$scope.currentModel.removeField = function(index) {
+		$scope.currentModel.fields.splice(index, 1);
+	}
 }]);
