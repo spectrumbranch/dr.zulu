03d54381467f1f2d1f8edf486b4ee96eb814377b
Christopher Bebry <invalidsyntax@gmail.com>
2014-12-04 00:00:31 -0500

    Fixed up some whitespace.

--- static/js/zuluCtrl.js
+++ static/js/zuluCtrl.js
@@ -1,31 +1,31 @@
 angular.module('zuluApp', ['zuluApp.model']).controller('ZuluController', ['$scope', 'Model', function($scope, Model) {
-	//maybe rename or reorganize this variable
-	$scope.constants = Model.constants;
-	
-	$scope.constants.associationTypes = [
-		{ id: 0, name: 'hasOne' },
+    //maybe rename or reorganize this variable
+    $scope.constants = Model.constants;
+    
+    $scope.constants.associationTypes = [
+        { id: 0, name: 'hasOne' },
         { id: 1, name: 'belongsTo' },
         { id: 2, name: 'hasMany' }
-	];
+    ];
 
-	$scope.currentProject = { name: 'Cartography', id: 0, editModel: false };
-	$scope.currentProject.toggleEditMode = function() {
-		$scope.currentProject.editMode = !$scope.currentProject.editMode;
-	}
-	$scope.currentProject.addNewModel = function() {
-		var id = $scope.currentProject.models.length; //TODO temporary id situation
-		$scope.currentProject.models.push(new Model({ name: 'NewModel' + id, id: id }));
-	}
-	$scope.currentProject.models = [];
+    $scope.currentProject = { name: 'Cartography', id: 0, editModel: false };
+    $scope.currentProject.toggleEditMode = function() {
+        $scope.currentProject.editMode = !$scope.currentProject.editMode;
+    }
+    $scope.currentProject.addNewModel = function() {
+        var id = $scope.currentProject.models.length; //TODO temporary id situation
+        $scope.currentProject.models.push(new Model({ name: 'NewModel' + id, id: id }));
+    }
+    $scope.currentProject.models = [];
     //TODO remove after development/testing. models should default to none
-	$scope.currentProject.models.push(new Model({ name: 'Map', id: 0 }));
-	$scope.currentProject.models.push(new Model({ name: 'Tile', id: 1 }));
-	
-	$scope.currentProject.associations = [];
+    $scope.currentProject.models.push(new Model({ name: 'Map', id: 0 }));
+    $scope.currentProject.models.push(new Model({ name: 'Tile', id: 1 }));
+    
+    $scope.currentProject.associations = [];
     //TODO remove after development/testing. associations should default to none
     $scope.currentProject.associations.push({ sourceModel: $scope.currentProject.models[0], targetModel: $scope.currentProject.models[1], associationType: $scope.constants.associationTypes[2] });
-	$scope.currentProject.associations.push({ sourceModel: $scope.currentProject.models[1], targetModel: $scope.currentProject.models[0], associationType: $scope.constants.associationTypes[1] });
-	
+    $scope.currentProject.associations.push({ sourceModel: $scope.currentProject.models[1], targetModel: $scope.currentProject.models[0], associationType: $scope.constants.associationTypes[1] });
+    
     $scope.currentProject.showAssociations = true;
     $scope.currentProject.toggleShowAssociations = function() {
         $scope.currentProject.showAssociations = !$scope.currentProject.showAssociations;
@@ -37,13 +37,13 @@ angular.module('zuluApp', ['zuluApp.model']).controller('ZuluController', ['$sco
         $scope.currentProject.associations.push({ sourceModel: {}, targetModel: {}, associationType: {} });
     }
     
-	$scope.toJson = function() {
-		var cleanProject = {};
-		cleanProject.name = $scope.currentProject.name;
-		cleanProject.models = [];
-		for (var i = 0; i < $scope.currentProject.models.length; i++) {
-			cleanProject.models.push($scope.currentProject.models[i].getCleanRepresentation());
-		}
-		console.log(cleanProject);
-	}
+    $scope.toJson = function() {
+        var cleanProject = {};
+        cleanProject.name = $scope.currentProject.name;
+        cleanProject.models = [];
+        for (var i = 0; i < $scope.currentProject.models.length; i++) {
+            cleanProject.models.push($scope.currentProject.models[i].getCleanRepresentation());
+        }
+        console.log(cleanProject);
+    }
 }]);
--- static/js/zuluModelFactory.js
+++ static/js/zuluModelFactory.js
@@ -1,99 +1,99 @@
 angular.module('zuluApp.model', []).factory('Model', [function ModelFactory() {
-	//Constructor
-	var Model = function(input) {
-		//input fields
-		this.name = input.name;
-		this.id = input.id;
-		
-		//default fields
-		//view status fields
-		this.editMode = false;
-		this.active = false;
-		//data fields
-		this.fields = [];
-		
-		//TODO remove
-		this.fields.push({ name: 'name', type: Model.constants.dataTypes[0], length: '10', allowNull: false });
-	};
-	
-	Model.constants = {};
-	Model.constants.dataTypes = [
-		{ name: "STRING", lengthField: true, isText: true },
-		{ name: "INTEGER", lengthField: true, unsignable: true, zerofillable: true, isInt: true },
-		{ name: "BIGINT", lengthField: true, unsignable: true, zerofillable: true, isInt: true },
-		{ name: "FLOAT", lengthField: true, decimalLengthField: true, unsignable: true, zerofillable: true, isDecimal: true },
-		{ name: "DECIMAL", lengthField: true, decimalLengthField: true, isDecimal: true },
-		{ name: "DATE", isDate: true },
-		{ name: "BOOLEAN", isBool: true },
-		{ name: "ENUM", isEnum: true }
-	];
-	
-	Model.constants.getDataTypeByObject = function(input) {
-		var output = {};
-		for (var i = 0; i < Model.constants.dataTypes.length; i++) {
-			if (input.name == Model.constants.dataTypes[i].name) {
-				output = Model.constants.dataTypes[i];
-				break;
-			}
-		}
-		return output;
-	};
-	
-	Model.prototype.toggleEditMode = function() {
-		this.editMode = !this.editMode;
-	};
-	
-	Model.prototype.toggleActive = function() {
-		this.active = !this.active;
-	};
-	
-	Model.prototype.addNewField = function() {
-		this.fields.push({ name: '', type: Model.constants.dataTypes[0], length: '' });
-	};
-	
-	Model.prototype.removeField = function(index) {
-		this.fields.splice(index, 1);
-	};
-	
-	Model.prototype.getCleanRepresentation = function() {
-		var output = {};
-		
-		output.name = this.name;
-		output.fields = [];
-		
-		for (var i = 0; i < this.fields.length; i++) {
-			var field = this.fields[i];
-			var cleanField = {};
-			
-			cleanField.allowNull = field.allowNull;
-			cleanField.name = field.name;
-			cleanField.type = field.type.name;
-			if (field.type.lengthField) {
-				cleanField.length = field.length;
-			}
-			
-			if (field.type.decimalLengthField) {
-				cleanField.decimalLength = field.decimalLength;
-			}
-			
-			if (field.type.unsignable) {
-				cleanField.unsigned = !!field.unsigned;
-			}
-			
-			if (field.type.zerofillable) {
-				cleanField.zerofill = !!field.zerofill;
-			}
-			
-			cleanField.hasDefaultValue = !!field.hasDefaultValue;
-			if (cleanField.hasDefaultValue) {
-				output.defaultValue = field.defaultValue;
-			}
-			
-			output.fields.push(cleanField);
-		}
-		
-		return output;
-	};
-	
-	return Model;
+    //Constructor
+    var Model = function(input) {
+        //input fields
+        this.name = input.name;
+        this.id = input.id;
+        
+        //default fields
+        //view status fields
+        this.editMode = false;
+        this.active = false;
+        //data fields
+        this.fields = [];
+        
+        //TODO remove
+        this.fields.push({ name: 'name', type: Model.constants.dataTypes[0], length: '10', allowNull: false });
+    };
+    
+    Model.constants = {};
+    Model.constants.dataTypes = [
+        { name: "STRING", lengthField: true, isText: true },
+        { name: "INTEGER", lengthField: true, unsignable: true, zerofillable: true, isInt: true },
+        { name: "BIGINT", lengthField: true, unsignable: true, zerofillable: true, isInt: true },
+        { name: "FLOAT", lengthField: true, decimalLengthField: true, unsignable: true, zerofillable: true, isDecimal: true },
+        { name: "DECIMAL", lengthField: true, decimalLengthField: true, isDecimal: true },
+        { name: "DATE", isDate: true },
+        { name: "BOOLEAN", isBool: true },
+        { name: "ENUM", isEnum: true }
+    ];
+    
+    Model.constants.getDataTypeByObject = function(input) {
+        var output = {};
+        for (var i = 0; i < Model.constants.dataTypes.length; i++) {
+            if (input.name == Model.constants.dataTypes[i].name) {
+                output = Model.constants.dataTypes[i];
+                break;
+            }
+        }
+        return output;
+    };
+    
+    Model.prototype.toggleEditMode = function() {
+        this.editMode = !this.editMode;
+    };
+    
+    Model.prototype.toggleActive = function() {
+        this.active = !this.active;
+    };
+    
+    Model.prototype.addNewField = function() {
+        this.fields.push({ name: '', type: Model.constants.dataTypes[0], length: '' });
+    };
+    
+    Model.prototype.removeField = function(index) {
+        this.fields.splice(index, 1);
+    };
+    
+    Model.prototype.getCleanRepresentation = function() {
+        var output = {};
+        
+        output.name = this.name;
+        output.fields = [];
+        
+        for (var i = 0; i < this.fields.length; i++) {
+            var field = this.fields[i];
+            var cleanField = {};
+            
+            cleanField.allowNull = field.allowNull;
+            cleanField.name = field.name;
+            cleanField.type = field.type.name;
+            if (field.type.lengthField) {
+                cleanField.length = field.length;
+            }
+            
+            if (field.type.decimalLengthField) {
+                cleanField.decimalLength = field.decimalLength;
+            }
+            
+            if (field.type.unsignable) {
+                cleanField.unsigned = !!field.unsigned;
+            }
+            
+            if (field.type.zerofillable) {
+                cleanField.zerofill = !!field.zerofill;
+            }
+            
+            cleanField.hasDefaultValue = !!field.hasDefaultValue;
+            if (cleanField.hasDefaultValue) {
+                output.defaultValue = field.defaultValue;
+            }
+            
+            output.fields.push(cleanField);
+        }
+        
+        return output;
+    };
+    
+    return Model;
 }]);
