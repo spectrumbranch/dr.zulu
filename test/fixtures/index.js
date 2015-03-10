5bcb632aabff849c26c1854c7a11b9789c6868ea
Christopher Bebry <invalidsyntax@gmail.com>
2015-03-10 00:07:38 -0400

    Added model and did some refactoring.

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -7,7 +7,7 @@ var fs = require('fs')
 var fixtures = {};
 fixtures.generator = {};
 
-fixtures.generator.model_index = [];
+
 
 fixtures.util = {};
 
@@ -38,6 +38,8 @@ fixtures.generator.initGenerator = function(name, cb) {
     fs.readdir(path.resolve(__dirname, './' + name), function(err, files) {
         if (err) throw err;
         var _files = files.filter(function(file) { return file != 'inputs'; }); 
+        
+        fixtures.generator[name] = [];
         async.each(_files, function(file, done) {
             fs.readFile(path.resolve(__dirname, './' + name + '/' + file), function(readErr, output) {
                 if (readErr) throw readErr;
@@ -55,6 +57,12 @@ fixtures.generator.initGenerator = function(name, cb) {
 
 fixtures.generator.initModelIndex = function(cb) {
     fixtures.generator.initGenerator('model_index', function(err, fixtures) {
+        cb(err, fixtures);
+    });
+};
+
+fixtures.generator.initModel = function(cb) {
+    fixtures.generator.initGenerator('model', function(err, fixtures) {
         cb(err, fixtures);
     });
 };
--- test/generator_tests.js
+++ test/generator_tests.js
@@ -36,4 +36,34 @@ suite('generator', function() {
             });
         })
     })
+    
+    
+    suite('renderModel', function() {
+        setup(function(done) {
+            fixtures.generator.initModel(function() {
+                done();
+            });
+        });
+        test('should match expected output fixtures', function(done) {
+            //fixtures.generator.model is an array of {name, input, output}
+            hoek.assert(fixtures.generator.model.length > 0, 'No renderModel fixtures exist.');
+            async.each(fixtures.generator.model, function(fixture, fixtureDone) {
+                generator.renderModel(fixture.input, function(err, output) {
+                    hoek.assert(err === null, 'Error from generator.renderModel() for fixture test: ' + fixture.name + ' ' );
+                    hoek.assert(output.length == 1, 'Length of output expected to equal 1');
+                    
+                    //test if output is what we expect
+                    fixture.output = fixture.output.toString();
+                    output = output[0].data;
+                    var theyMatch = fixture.output == output;
+                    if (!theyMatch) fixtures.util.displayDiff(fixture.output,output);
+                    hoek.assert(theyMatch, 'Mismatch output generator.renderModel() for fixture test: ' + fixture.name + '\n');
+                    
+                    fixtureDone();
+                });
+            }, function(err) {
+                done();
+            });
+        })
+    })
 })
